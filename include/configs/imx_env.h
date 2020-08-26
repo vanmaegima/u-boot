bd348d7cda5a9dc91d6d2fba6c3964ce314128e0
Ricardo Salveti <ricardo@foundries.io>
2020-08-26 16:18:34 -0300

    [FIO internal] imx_env: set mfg to run fastboot by default
    We don't support any other mfg boot mechanism other than fastboot, so
    jump to that by default instead of trying to load a custom kernel.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>
    Co-developed-by: Michael Scott <mike@foundries.io>
    Signed-off-by: Michael Scott <mike@foundries.io>
    Co-developed-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
    (cherry picked from commit 67e67bf16be1e6c3980bae382d0cd304fc49595a)

--- include/configs/imx_env.h
+++ include/configs/imx_env.h
@@ -57,17 +57,9 @@
 		"rdinit=/linuxrc " \
 		"clk_ignore_unused "\
 		"\0" \
-	"kboot="MFG_BOOT_CMD"\0"\
 	"bootcmd_mfg=run mfgtool_args;" \
-        "if iminfo ${initrd_addr}; then " \
-            "if test ${tee} = yes; then " \
-                "bootm ${tee_addr} ${initrd_addr} ${fdt_addr}; " \
-            "else " \
-                MFG_BOOT_CMD "${loadaddr} ${initrd_addr} ${fdt_addr}; " \
-            "fi; " \
-        "else " \
-		FASTBOOT_CMD  \
-        "fi;\0" \
+	FASTBOOT_CMD \
+	"\0" \
 	MFG_NAND_FIT_PARTITION \
 
 #endif
