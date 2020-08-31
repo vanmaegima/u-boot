9bd13e9d7944c5113d68579d14a7275df427bde8
Ricardo Salveti <ricardo@foundries.io>
2020-08-31 19:14:10 -0300

    [FIO toup] imx: add base SPL support for mx7ulp
    Base mx7ulp SPL config header, base soc and makefile changes in order to
    allow building SPL on mx7ulp based devices.
    
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- arch/arm/mach-imx/spl.c
+++ arch/arm/mach-imx/spl.c
@@ -115,7 +115,8 @@ u32 spl_boot_device(void)
 	return BOOT_DEVICE_NONE;
 }
 
-#elif defined(CONFIG_MX7) || defined(CONFIG_IMX8M) || defined(CONFIG_IMX8)
+#elif defined(CONFIG_MX7) || defined(CONFIG_MX7ULP) || \
+	defined(CONFIG_IMX8M) || defined(CONFIG_IMX8)
 /* Translate iMX7/i.MX8M boot device to the SPL boot device enumeration */
 u32 spl_boot_device(void)
 {
@@ -128,7 +129,9 @@ u32 spl_boot_device(void)
 	 */
 	if (((bmode >> 24) & 0x03) == 0x01) /* Serial Downloader */
 		return BOOT_DEVICE_BOARD;
+#endif
 
+#if defined(CONFIG_MX7) || defined(CONFIG_MX7ULP)
 	/*
 	 * The above method does not detect that the boot ROM used
 	 * serial downloader in case the boot ROM decided to use the
@@ -156,6 +159,10 @@ u32 spl_boot_device(void)
 	case MMC2_BOOT:
 	case SD3_BOOT:
 	case MMC3_BOOT:
+		return BOOT_DEVICE_MMC1;
+#elif defined(CONFIG_MX7ULP)
+	case SD1_BOOT:
+	case MMC1_BOOT:
 		return BOOT_DEVICE_MMC1;
 #elif defined(CONFIG_IMX8)
 	case MMC1_BOOT:
@@ -186,7 +193,7 @@ u32 spl_boot_device(void)
 		return BOOT_DEVICE_NONE;
 	}
 }
-#endif /* CONFIG_MX7 || CONFIG_IMX8M || CONFIG_IMX8 */
+#endif /* CONFIG_MX7 || CONFIG_MX7ULP || CONFIG_IMX8M || CONFIG_IMX8 */
 
 #ifdef CONFIG_SPL_USB_GADGET
 int g_dnl_bind_fixup(struct usb_device_descriptor *dev, const char *name)
