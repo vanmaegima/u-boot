169a6034047d51cee33e14bf3f7406a2c5bf611d
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2020-11-19 12:12:45 +0100

    [FIO tonxp] fastboot: fix build warnings
    Fix the following annoying build warnings [1].
    
    rsalveti: serial usage depends on two configs, so make sure they are
    both covered at the variable declaration to avoid build failures.
    
    [1]
    drivers/fastboot/fb_fsl/fb_fsl_common.c: In function ‘fastboot_setup’:
    drivers/fastboot/fb_fsl/fb_fsl_common.c:359:2: warning: ‘serialnr.high’ is used uninitialized in this function [-Wuninitialized]
      359 |  sprintf(serial, "%08x%08x", serialnr.high, serialnr.low);
          |  ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    drivers/fastboot/fb_fsl/fb_fsl_common.c:359:2: warning: ‘serialnr.low’ is used uninitialized in this function [-Wuninitialized]
    drivers/fastboot/fb_fsl/fb_fsl_getvar.c:117:13: warning: ‘serial’ defined but not used [-Wunused-variable]
      117 | static char serial[IMX_SERIAL_LEN];
          |             ^~~~~~
    
    Upstream-Status: Submitted [directly to Jason Liu <jason.hui.liu@nxp.com>]
    Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>
    Co-developed-by: Ricardo Salveti <ricardo@foundries.io>
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
    (cherry picked from commit ec4f60ea6e17572cfa030fcbc88c1ad5a71742f8)

--- drivers/fastboot/fb_fsl/fb_fsl_getvar.c
+++ drivers/fastboot/fb_fsl/fb_fsl_getvar.c
@@ -114,7 +114,9 @@ static bool is_slotvar(char *cmd)
 	return false;
 }
 
+#if defined(CONFIG_SERIAL_TAG) || defined(CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG)
 static char serial[IMX_SERIAL_LEN];
+#endif
 
 char *get_serial(void)
 {
