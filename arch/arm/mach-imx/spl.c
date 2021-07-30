3dabecc4668af33e5ab34a18d3b2ea0f2c94c15e
Igor Opaniuk <igor.opaniuk@foundries.io>
2021-07-30 18:32:30 +0300

    [FIO internal] imx8: use different address for FIT buffer in board_spl_fit_buffer_addr()
    Calculation of buffer address for loading FIT image seems to work for imx8m,
    but not for other SoCs like iMX8QM, where CONFIG_SYS_TEXT_BASE=0x80020000,
    As 0x20000 is not sufficient space for that.
    
    Default memory layout for iMX8QM is:
    * SYS_SDRAM_BASE       0x80000000      0.125MiB
    * SYS_TEXT_BASE        0x80020000      2.375MiB
    * kernel_addr_r        0x80280000      45.5MiB
    * fdt_addr_r           0x83000000      1MiB
    
    Use fdt_addr_r for that instead to have a big enough location not
    overlapped with U-boot image.
    
    This fixes corruption of FIT image on iMX8QM:
    spl_load_simple_fit: Cannot find /images node: -4
    
    Also move that function outside of CONFIG_SPL_IMX_HAB define, as
    it can be used also when SPL HAB support is not enabled.
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>
    Co-developed-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
    (cherry picked from commit 582c254cf3722d7f05984bcc2eb4dd18308820c1)

--- arch/arm/mach-imx/spl.c
+++ arch/arm/mach-imx/spl.c
@@ -345,10 +345,12 @@ ulong board_spl_fit_size_align(ulong size)
 	return size;
 }
 #endif
+#endif
 
 void *board_spl_fit_buffer_addr(ulong fit_size, int sectors, int bl_len)
 {
 	int align_len = ARCH_DMA_MINALIGN - 1;
+	uintptr_t base_addr;
 
 	/* Some devices like SDP, NOR, NAND, SPI are using bl_len =1, so their fit address
 	 * is different with SD/MMC, this cause mismatch with signed address. Thus, adjust
@@ -357,10 +359,14 @@ void *board_spl_fit_buffer_addr(ulong fit_size, int sectors, int bl_len)
 	if (bl_len < 512)
 		bl_len = 512;
 
-	return  (void *)((CONFIG_TEXT_BASE - fit_size - bl_len -
+	if (is_imx8qm() || is_imx8qxp() || is_imx8dxl())
+		base_addr = 0x83000000;
+	else
+		base_addr = CONFIG_TEXT_BASE;
+
+	return (void *)((base_addr - fit_size - bl_len -
 			align_len) & ~align_len);
 }
-#endif
 
 #if IS_ENABLED(CONFIG_SPL_LOAD_FIT)
 static int spl_verify_fit_hash(const void *fit)
