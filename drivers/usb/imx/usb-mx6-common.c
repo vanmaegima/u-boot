cff38e142804aee0621bf3c8546b60fcd4e56c22
Ye Li <ye.li@nxp.com>
2021-08-25 16:08:42 +0800

    [FIO fromimx] LF-4375 usb: imx: Fix USB charger detector issue
    Remove the USB PHY charger detector setting on iMX8, iMX8ULP,
    and iMX7ULP. This register has reserved bit31, bit20 and bit19
    to 1. By default it selects USBHSDCD module for USB charger
    detection, but the setting clears the bit 31 to use PHY charger
    detector which cause kernel failed to recognize the charger type.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Jun Li <jun.li@nxp.com>
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
    (cherry picked from commit 2df02e6aab574f780d90a068140dd258411d80e4)

--- drivers/usb/imx/usb-mx6-common.c
+++ drivers/usb/imx/usb-mx6-common.c
@@ -87,10 +87,6 @@ static void usb_power_config(int index)
 	struct usbphy_regs __iomem *usbphy =
 		(struct usbphy_regs __iomem *)USB_PHY0_BASE_ADDR;
 
-	writel(ANADIG_USB2_CHRG_DETECT_EN_B |
-		   ANADIG_USB2_CHRG_DETECT_CHK_CHRG_B,
-		   &usbphy->usb1_chrg_detect);
-
 	scg_enable_usb_pll(true);
 
 #elif defined(CONFIG_IMX8)
@@ -99,10 +95,6 @@ static void usb_power_config(int index)
 	struct usbphy_regs __iomem *usbphy = (struct usbphy_regs __iomem *)phy_bases[index];
 
 	int timeout = 1000000;
-
-	writel(ANADIG_USB2_CHRG_DETECT_EN_B |
-		   ANADIG_USB2_CHRG_DETECT_CHK_CHRG_B,
-		   &usbphy->usb1_chrg_detect);
 
 	if (!(readl(&usbphy->usb1_pll_480_ctrl) & PLL_USB_LOCK_MASK)) {
 
