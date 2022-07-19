76869c9c63444bd474d31f8aa76b76f24a424984
Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
2022-07-19 16:17:37 +0300

    [FIO internal] hash: disable crc16-ccitt if FIT_SIGNATURE_STRICT
    FIT_SIGNATURE_STRICT requires to use high-secure hash algorithms for
    signing FIT images. Disable the weakest algorithm CRC16 to avoid a
    possible attack vector.
    
    Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>
    (cherry picked from commit bf08d56c0aa4a2abda07fff80dcfc1f8711a6669)
    Signed-off-by: Vanessa Maegima <vanessa.maegima@foundries.io>

--- common/hash.c
+++ common/hash.c
@@ -144,7 +144,7 @@ static int __maybe_unused hash_finish_sha512(struct hash_algo *algo, void *ctx,
 	return 0;
 }
 
-static int hash_init_crc16_ccitt(struct hash_algo *algo, void **ctxp)
+static int __maybe_unused hash_init_crc16_ccitt(struct hash_algo *algo, void **ctxp)
 {
 	uint16_t *ctx = malloc(sizeof(uint16_t));
 	*ctx = 0;
@@ -152,7 +152,7 @@ static int hash_init_crc16_ccitt(struct hash_algo *algo, void **ctxp)
 	return 0;
 }
 
-static int hash_update_crc16_ccitt(struct hash_algo *algo, void *ctx,
+static int __maybe_unused hash_update_crc16_ccitt(struct hash_algo *algo, void *ctx,
 				   const void *buf, unsigned int size,
 				   int is_last)
 {
@@ -160,7 +160,7 @@ static int hash_update_crc16_ccitt(struct hash_algo *algo, void *ctx,
 	return 0;
 }
 
-static int hash_finish_crc16_ccitt(struct hash_algo *algo, void *ctx,
+static int __maybe_unused hash_finish_crc16_ccitt(struct hash_algo *algo, void *ctx,
 				   void *dest_buf, int size)
 {
 	if (size < algo->digest_size)
@@ -296,6 +296,7 @@ static struct hash_algo hash_algo[] = {
 #endif
 	},
 #endif
+#if CONFIG_IS_ENABLED(CRC16)
 	{
 		.name		= "crc16-ccitt",
 		.digest_size	= 2,
@@ -305,6 +306,7 @@ static struct hash_algo hash_algo[] = {
 		.hash_update	= hash_update_crc16_ccitt,
 		.hash_finish	= hash_finish_crc16_ccitt,
 	},
+#endif
 #if CONFIG_IS_ENABLED(CRC32)
 	{
 		.name		= "crc32",
