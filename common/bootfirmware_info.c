32dd1f0e5fec310f672110d62fafb2552387b6d2
Igor Opaniuk <igor.opaniuk@foundries.io>
2023-03-30 18:14:59 +0300

    [FIO internal] common: introduce BOOTFIRMWARE_INFO_STRICT
    Introduce BOOTFIRMWARE_INFO_STRICT Kconfig symbol, which can control
    behavior of U-Boot if error occurs during firmware version detection.
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>
    (cherry picked from commit 08452551bfa6fad7af475757be88d9cb0879eeff)
    (cherry picked from commit eb020343591965440da887ce08665a7c23374b3b)

--- common/bootfirmware_info.c
+++ common/bootfirmware_info.c
@@ -14,12 +14,14 @@ int __weak get_boot_firmware_info(void)
 {
 	char *version;
 	int node;
+	int ret = 0;
 
 	node = fdt_node_offset_by_compatible(gd->fdt_blob, -1, COMPATIBLE);
 	if (node < 0) {
 		printf("Can't find node with compatible = \"" COMPATIBLE
 		       "\"\n");
-		return -FDT_ERR_NOTFOUND;
+		ret = -FDT_ERR_NOTFOUND;
+		goto out;
 	}
 
 	version = fdt_getprop(gd->fdt_blob, node, "bootfirmware-version", NULL);
@@ -27,7 +29,15 @@ int __weak get_boot_firmware_info(void)
 		printf("Boot firmware version: %s\n", version);
 		env_set("dt_bootfirmware_version", version);
 	} else {
-		return -FDT_ERR_NOTFOUND;
+		ret = -FDT_ERR_NOTFOUND;
+	}
+
+out:
+	/* we should stop booting, if boot firmware version is not found */
+	if (CONFIG_IS_ENABLED(BOOTFIRMWARE_INFO_STRICT) && ret != 0) {
+		printf("Fail to read boot firmware info from DTB, "
+		       "abort boot as BOOTFIRMWARE_INFO_STRICT is set\n");
+		return ret;
 	}
 
 	return 0;
